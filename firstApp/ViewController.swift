f9cb7d677c7700bd2b87f40999371d58e271b180
guilherme <ext.pablo.cunha@motz.com.br>
2022-06-15 14:19:07 -0300

    initial commit

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var _nomeTextField: UITextField!
+    @IBOutlet weak var _nomeLabel: UILabel!
+    @IBOutlet var _sobrenomeTextField: UITextField!
+    
+    @IBAction func Enviar(_ sender: Any) {
+        switch _sobrenomeTextField.text! {
+        case "Bernava" :
+            print("Salve perninha")
+        case "Genuino" :
+            print("Salve genio")
+        default:
+            print("Salve meu default")
+        }
+        _nomeLabel.text = _nomeTextField.text! + " " + _sobrenomeTextField.text!
     }
-
-
 }
 
